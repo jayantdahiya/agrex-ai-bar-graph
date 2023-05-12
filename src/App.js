83abf5381052659f6aeb59a6a602bd82e11bbaa2
Jayant Dahiya <jayantdahiya1999@gmail.com>
2023-05-12 15:13:43 +0530

    Initial CRA | tailwind setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className='m-auto'>
+        <p className='text-2xl'>Bar Graph</p>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
